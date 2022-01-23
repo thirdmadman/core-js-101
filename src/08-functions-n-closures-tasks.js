0bbf5e8e784b1993efb050ff814c0896b95c8aa1
thirdmadman <thirdmadman@gmail.com>
2022-01-23 18:35:46 +0400

    feat: impl logger

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -147,8 +147,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsToString = (...inputArgs) => inputArgs.map((el) => JSON.stringify(el)).join(',');
+    logFunc(`${func.name}(${argsToString(...args)}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argsToString(...args)}) ends`);
+    return result;
+  };
 }
 
 
